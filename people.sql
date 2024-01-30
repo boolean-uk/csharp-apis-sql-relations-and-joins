17da32d90c782f13e99ce0d5e464816e9c5f9fba
henrikrosenkilde <henrik.rosenkilde@hotmail.com>
2024-01-30 15:14:18 +0100

    people lol

---
+++ people.sql
@@ -0,0 +1,51 @@
+-- This script only contains the table creation statements and does not fully represent the table in the database. It's still missing: indices, triggers. Do not use it as a backup.
+
+-- Sequence and defined type
+CREATE SEQUENCE IF NOT EXISTS people_person_id_seq;
+
+-- Table Definition
+CREATE TABLE "public"."people" (
+    "person_id" int4 NOT NULL DEFAULT nextval('people_person_id_seq'::regclass),
+    "role_id" int4,
+    "name" varchar(100),
+    "date_of_birth" date,
+    "email" varchar(255),
+    "country" varchar(255),
+    CONSTRAINT "people_role_id_fkey" FOREIGN KEY ("role_id") REFERENCES "public"."rules"("id"),
+    PRIMARY KEY ("person_id")
+);
+
+INSERT INTO "public"."people" ("person_id", "role_id", "name", "date_of_birth", "email", "country") VALUES
+(1, 2, 'Gregory Peck', '1916-04-05', NULL, NULL);
+INSERT INTO "public"."people" ("person_id", "role_id", "name", "date_of_birth", "email", "country") VALUES
+(2, 3, 'Arthur C Clarke', NULL, 'arthur@clarke.com', NULL);
+INSERT INTO "public"."people" ("person_id", "role_id", "name", "date_of_birth", "email", "country") VALUES
+(3, 2, 'Leonardo DiCaprio', '1974-11-11', NULL, NULL);
+INSERT INTO "public"."people" ("person_id", "role_id", "name", "date_of_birth", "email", "country") VALUES
+(4, 1, 'Anthony Mann', NULL, NULL, 'USA'),
+(5, 1, 'Krzysztof Kieslowski', NULL, NULL, 'Poland'),
+(6, 2, 'Julie Christie', '1940-04-14', NULL, NULL),
+(7, 3, 'Edmond Rostand', NULL, 'edmond@rostand.com', NULL),
+(8, 2, 'Rutger Hauer', '1944-01-23', NULL, NULL),
+(9, 3, 'Krzysztof Kieslowski', NULL, 'email@email.com', NULL),
+(10, 2, 'Juliette Binoche', '1964-03-09', NULL, NULL),
+(11, 1, 'Stanley Kubrick', NULL, NULL, 'USA'),
+(12, 2, 'Manos Katrakis', '1908-08-14', NULL, NULL),
+(13, 1, 'George Lucas', NULL, NULL, 'USA'),
+(14, 1, 'Jean-Paul Rappeneau', NULL, NULL, 'France'),
+(15, 2, 'Charlton Heston', '1923-10-04', NULL, NULL),
+(16, 3, 'Frederick Frank', NULL, 'fred@frank.com', NULL),
+(17, 3, 'Harper Lee', NULL, 'harper@lee.com', NULL),
+(18, 1, 'David Lean', NULL, NULL, 'UK'),
+(19, 3, 'Boris Pasternak', NULL, 'boris@boris.com', NULL),
+(20, 2, 'Keir Dullea', '1936-05-30', NULL, NULL),
+(21, 3, 'Theodoros Angelopoulos', NULL, 'theo@angelopoulos.com', NULL),
+(22, 2, 'Gerard Depardieu', '1948-12-27', NULL, NULL),
+(23, 2, 'Mark Hamill', '1951-09-25', NULL, NULL),
+(24, 3, 'Erik Hazelhoff Roelfzema', NULL, 'erik@roelfzema.com', NULL),
+(25, 3, 'George Lucas', NULL, 'george@email.com', NULL),
+(26, 1, 'Robert Mulligan', NULL, NULL, 'USA'),
+(27, 1, 'Paul Verhoeven', NULL, NULL, 'Netherlands'),
+(28, 1, 'Theodoros Angelopoulos', NULL, NULL, 'Greece'),
+(29, 3, 'James Cameron', NULL, 'james@cameron.com', NULL),
+(30, 1, 'James Cameron', NULL, NULL, 'Canada');
